402215d96fc75e481d0c23c237efb55f334a187f
dizzyfox734 <dizzyfox734@gmail.com>
2022-11-29 13:48:45 +0900

    Initial commit

---
+++ src/main/java/org/dizzyfox734/dockerpublishtest/web/HelloController.java
@@ -0,0 +1,13 @@
+package org.dizzyfox734.dockerpublishtest.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hello";
+    }
+}
